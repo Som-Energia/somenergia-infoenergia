fd131e482338b00ebbd22d68917a36e95ff9ba2d
Joana Figueira <joana.figueira@somenergia.coop>
2019-04-02 13:34:31 +0200

    init commit

---
+++ __terp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "test creación de un modulo",
+    "description": """Afegeix un test para Infoenergia""",
+    "version": "0-dev",
+    "author": "GISCE",
+    "category": "Som",
+    "depends":[
+        "base",
+        "giscedata_polissa",
+        "giscedata_cups"
+    ],
+    "init_xml": [],
+    "update_xml":[
+        "somenergia_infoenergia_view.xml"
+    ],
+    "active": False,
+    "installable": True
+}
